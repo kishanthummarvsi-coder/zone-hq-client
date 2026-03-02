1f0725fd9564dc46e939523d23110dd5372a05a4
Kishan Thummar <kishan.thummar@vsiinfotech.com>
2026-03-02 17:42:22 +0530

    fix

--- src/app/layout/components/sidebar/sidebar.ts
+++ src/app/layout/components/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,4 +9,10 @@ import { Component, Input } from '@angular/core';
 })
 export class SidebarComponent {
   @Input() isExpanded = true;
+
+  constructor(private router: Router) { }
+
+  go(path: string) {
+    this.router.navigateByUrl(path);
+  }
 }
